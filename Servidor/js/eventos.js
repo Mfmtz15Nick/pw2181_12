acc6178aa5b5dd2b500b542e34becc32b6693234
CCDM12 <mario_fonseca15@hotmail.com>
2018-05-22 13:06:50 -0600

    Login funcionando con PHP

---
+++ Servidor/js/eventos.js
@@ -0,0 +1,32 @@
+
+var inicioApp = function(){
+    var Aceptar = function(){
+        var usuario=$("txtUsuario").val();
+        var clave  =$("txtClave").val();
+        var parametros="opc=validaentrada"+
+                       "&usuario="+usuario+
+                       "&clave="+clave+
+                       "&aleatorio="+Math.random();
+        $.ajax({
+            cache:false,
+            type:"POST",
+            dataType: "json",
+            url: "php/validaentrada.php",
+            data: parametros,
+            success: function(response){
+                if(response.respuesta){
+                    alert("Bienvenido");
+                }else{
+                    alert("Error");
+                }
+
+            },
+            error: function(xhr,ajaxOptions,thrownError){
+
+            }
+        });
+    }
+    $("#btnAceptar").on("click",Aceptar);
+
+}
+$(document).ready(inicioApp);
