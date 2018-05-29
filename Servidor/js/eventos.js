61bf6321496f7be31ebbf19c203a8ab7fabc0e05
CCDM12 <mario_fonseca15@hotmail.com>
2018-05-29 13:06:45 -0600

    ABCC - Programado hasta Guardar

--- Servidor/js/eventos.js
+++ Servidor/js/eventos.js
@@ -1,109 +1,180 @@
-
 var inicioApp = function(){
     var Aceptar = function(){
         var usuario=$("#txtUsuario").val();
-        var clave  =$("#txtClave").val();
+        var clave=$("#txtClave").val();
+        
         var parametros="opc=validaentrada"+
                        "&usuario="+usuario+
                        "&clave="+clave+
                        "&aleatorio="+Math.random();
         $.ajax({
             cache:false,
-            type:"POST",
+            type: "POST",
             dataType: "json",
             url: "php/validaentrada.php",
             data: parametros,
             success: function(response){
-                if(response.respuesta==true){
-                    //Ocultamos inicio
-                    $("#secInicio").hide(("slow"));
-                    //Aparecer Usuario
+                if(response.respuesta == true){
+                    // alert("Bienvenido");
+                    //Ocultamos al inicio
+                    $("#secInicio").hide("slow");
+                    //Aparecemos usuarios
                     $("#frmUsuarios").show("slow");
                     //Cursor en el primer cuadro de texto
                     $("#txtNombreUsuario").focus();
+
                 }else{
-                    alert("Error");
+                    alert("Usuario o Clave incorrecta");
                 }
 
             },
-            error: function(xhr,ajaxOptions,thrownError){
+            error: function(xhr,ajaxOptions,throwError){
                 console.log(xhr);
-
+               
             }
         });
     }
+
     var buscarUsuario = function(){
         var usuario = $("#txtNombreUsuario").val();
-        var parametros = "opc=buscarUsuario"+
-                         "&usuario="+usuario+
-                         "&aleatorio="+Math.random();
+        var parametros="opc=buscarUsuario"+
+                       "&usuario="+usuario+
+                       "&aleatorio="+Math.random();
+
         if(usuario != ""){
             $.ajax({
                 cache:false,
-                type:"POST",
+                type: "POST",
                 dataType: "json",
                 url: "php/buscausuario.php",
                 data: parametros,
                 success: function(response){
                     if(response.respuesta == true){
+                        
                         $("#txtNombre").val(response.nombre);
-                        $("#txtClaveUsuario").val(reponse.clave);
-                       
+                        $("#txtClaveUsuario").val(response.clave);
                     }else{
                         $("#txtNombre").focus();
                     }
-                    
-    
+                 
                 },
-                error: function(xhr,ajaxOptions,thrownError){
+                error: function(xhr,ajaxOptions,throwError){
                     console.log(xhr);
-    
+                   
                 }
             });
 
         }
     }
+
     var teclaNombreUsuario = function(tecla){
-        if(tecla.which == 13){//Enter
+        if(tecla.which == 13){//A un Enter
             buscarUsuario();
         }
     }
     var Guardar = function(){
-        var usuario=$('#txtNombreUsuario').val();
-        var nombre =$('#txtNombre').val();
-        var clave =$('#txtClaveUsuario').val();
-        if(usuario !="" && nombre !="" && clave != ""){
-
-$.ajax({
-            cache:false,
-            type:"POST",
-            dataType: "json",
-            url: "php/guardarusuario.php",
-            data: parametros,
-            success: function(response){
-                if(response.respuesta==true){
-                    alert("Datos guardados correctamente");
-                    $("#frmUsuarios >input").val("");
-                    
-                }else{
-                    alert("Ocurrio un error, intente de nuevo mas tarde");
+        var usuario=$("#txtNombreUsuario").val();
+        var nombre=$("#txtNombre").val();
+        var clave=$("#txtClaveUsuario").val();
+        var parametros="opc=guardarUsuario"+
+                        "&usuario="+usuario+
+                        "&clave="+clave+
+                        "&nombre="+nombre+
+                        "&aleatorio="+Math.random();
+        if(usuario!="" && nombre!="" && clave!=""){
+            $.ajax({
+                cache:false,
+                type: "POST",
+                dataType: "json",
+                url: "php/guardarusuario.php",
+                data: parametros,
+                success: function(response){
+                    if(response.respuesta == true){
+                        alert("Datos guardados correctamente");
+                        $("#frmUsuarios > input").val("");
+                    }else{
+                        alert("entra aqui :)")
+                    }
+                },
+                error: function(xhr,ajaxOptions,throwError){
+                    console.log(xhr);
+                   
                 }
-
-            },
-            error: function(xhr,ajaxOptions,thrownError){
-                console.log(xhr);
-
-            }
-        });
-
+            });
 
         }else{
             alert("Llene todos los campos");
         }
+        
     }
+
+    var Borrar = function(){
+        var usuario = $("txtNombreUsuario").val();
+        var nombre=$("#txtNombre").val();
+        var pregunta = prompt("Seguro que desea borrar a"+nombre+"? (si/no)","no");
+        var parametros = "opc=guardarUsuario"+
+        "&usuario="+usuario+
+        "&clave="+clave+
+        "&nombre="+nombre+
+        "&aleatorio="+Math.random();
+        if(pregunta != null && pregunta == "si"){
+            //Aqui va el AJAX.
+            $.ajax({
+                cache:false,
+                type: "POST",
+                dataType: "json",
+                url: "php/borrarusuario.php",
+                data: parametros,
+                success: function(response){
+                    if(response.respuesta == true){
+                        alert("Datos borrados correctamente");
+                        $("#frmUsuarios > input").val("");
+                    }else{
+                        alert("ocurrio un error, Intente de nuevo mas tarde")
+                    }
+                },
+                error: function(xhr,ajaxOptions,throwError){
+                    console.log(xhr);
+                   
+                }
+            });
+        }
+
+    }
+
+    var Listado = function(){
+        $("main > section").hide("slow");
+        $("frmListado").show("slow");
+        var parametros = "opc=listado"+
+                         "$aleatorio="+Math.random();
+        $.ajax({
+            cache:false,
+            type: "POST",
+            dataType: "json",
+            url: "php/Listado.php",
+            data: parametros,
+            success: function(response){
+                if(response.respuesta == true){
+                   
+                }else{
+                    alert("ocurrio un error, Intente de nuevo mas tarde")
+                }
+            },
+            error: function(xhr,ajaxOptions,throwError){
+                console.log(xhr);
+               
+            }
+        });
+
+    }
+
+
     $("#btnAceptar").on("click",Aceptar);
     $("#txtNombreUsuario").on("keypress",teclaNombreUsuario);
     $("#btnGuardar").on("click",Guardar);
+    $("#btnBorrar").on("click",Borrar);
+    $("btnListado").on("click",Listado);
 
 }
+
 $(document).ready(inicioApp);
--- Servidor/php/guardarusuario.php
+++ Servidor/php/guardarusuario.php
@@ -1,43 +1,42 @@
 <?php
 include 'conexiones.php';
 function guardarusuario(){
-    $respuesta=false;
-    $usuario=GetSQLValueString($_POST["usuario"],"text");
-    $nombre=GetSQLValueString($_POST["nombre"],"text");
+    $respuesta = false;
+    $usuario= GetSQLValueString($_POST["usuario"],"text");
+    $nombre= GetSQLValueString($_POST["nombre"],"text");
     $clave=GetSQLValueString(md5($_POST["clave"]),"text");
     
-
-   
-    //Conectarnosal servidor de BD.
+    
+    //Conectarnos al servidor de la BD.
     $con=conecta();
-    //$consulta="select usuario from usuarios where usuario= '".$usuario."' limit 1";
-    $consulta=printf("select usuario from usuarios where usuario = %s",$usuario);
+    // $consulta="select usuario from usuarios where usuario= '".$usuario."' limit 1";
+    $consulta=sprintf("select usuario from usuarios where usuario=%s",$usuario);
     $resConsulta=mysqli_query($con,$consulta);
     $consultaGuarda ="";
     //Si ya existe en la tabla el usuario
-    
-   
     if(mysqli_num_rows($resConsulta) > 0){
-        $consultaGuarda=sprintf("update usuarios set nombre = %s,clave = %s where usuario = %s",$nombre,$clave,$usuario);
-        
-    }else{//No existe en la tabla
-        $consultaGuarda=sprintf("insert into usuarios values(default,%s,%s,%s)",$usuario,$nombre,$clave);
+        //Actualizamos
+        $consultaGuarda=sprintf("update usuarios set nombre =%s,clave =%s where usuario =%s",$nombre,$clave,$usuario);
+        }else{//No existe en la tabla
+            $consultaGuarda=sprintf("insert into usuarios values(default,%s,%s,%s)",$usuario,$nombre,$clave);
+        }
+    
+    mysqli_query($con,$consultaGuarda);//Ejecuta la consulta
+    if(mysqli_affected_rows($con)>0){
+        $respuesta == true;
     }
-    mysqli_query($con,$consultaGuarda);//ejecuta la consulta
-    if(mysqli_affected_rows()>0){
-        $respuesta = true;
-    }
+    //Array asociativo
     $salidaJSON = array('respuesta' => $respuesta);
     print json_encode($salidaJSON);
 }
 $opc=$_POST["opc"];
 switch ($opc) {
     case 'guardarUsuario':
-        guardarusuario();
+         guardarusuario();
         break;
-    
+        
     default:
-        # code...
+         # code...
         break;
 }
 ?>
